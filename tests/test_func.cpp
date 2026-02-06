5336b1bab12a396df90494bdf3fa54b8d20a7780
unknown <baudin-thomas@sfr.fr>
2026-02-06 17:54:31 +0100

    idem

--- tests/test_func.cpp
+++ tests/test_func.cpp
@@ -1,10 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
-// #include "Func.hpp"
+#include "Func.hpp"
 
-// TEST_CASE("Colors", "[colors]") {
-//     REQUIRE(segment01::Func::colorBalance(sf::Color(0, 0, 0, 255), sf::Color(255, 255, 255, 255), 0.5) == sf::Color(127, 127, 127, 255));
-// }
-
-TEST_CASE("Sanity", "[sanity]") {
-    REQUIRE(2 + 2 == 5);
+TEST_CASE("Colors", "[colors]") {
+    REQUIRE(segment01::Func::colorBalance(sf::Color(0, 0, 0, 255), sf::Color(255, 255, 255, 255), 0.5) == sf::Color(127, 127, 127, 255));
 }
+
+// TEST_CASE("Sanity", "[sanity]") {
+//     REQUIRE(2 + 2 == 4);
+// }
