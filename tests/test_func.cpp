f2be202b814ccea3a7df89910dc7683f60ad82b7
unknown <baudin-thomas@sfr.fr>
2026-02-06 17:23:36 +0100

    idem

--- tests/test_func.cpp
+++ tests/test_func.cpp
@@ -1,5 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
-#include "Func.hpp"
+// #include "Func.hpp"
 
 // TEST_CASE("Colors", "[colors]") {
 //     REQUIRE(segment01::Func::colorBalance(sf::Color(0, 0, 0, 255), sf::Color(255, 255, 255, 255), 0.5) == sf::Color(127, 127, 127, 255));
