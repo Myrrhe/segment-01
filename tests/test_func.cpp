adf9dd5088638b93744afe7176a06a9d25465e84
unknown <baudin-thomas@sfr.fr>
2026-02-06 16:35:40 +0100

    idem

--- tests/test_func.cpp
+++ tests/test_func.cpp
@@ -3,5 +3,4 @@
 
 TEST_CASE("Colors", "[colors]") {
     REQUIRE(segment01::Func::colorBalance(sf::Color(0, 0, 0, 255), sf::Color(255, 255, 255, 255), 0.5) == sf::Color(127, 127, 127, 255));
-    std::cout << "machin" << std::endl;
 }
