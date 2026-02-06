5827bec8b1af36e884ddc3b0ea98399fca8b93a4
unknown <baudin-thomas@sfr.fr>
2026-02-06 16:52:25 +0100

    idem

--- tests/test_func.cpp
+++ tests/test_func.cpp
@@ -5,6 +5,6 @@
 //     REQUIRE(segment01::Func::colorBalance(sf::Color(0, 0, 0, 255), sf::Color(255, 255, 255, 255), 0.5) == sf::Color(127, 127, 127, 255));
 // }
 
-TEST_CASE("Sanity", "[sanity]") {
-    REQUIRE(2 + 2 == 4);
-}
+// TEST_CASE("Sanity", "[sanity]") {
+//     REQUIRE(2 + 2 == 4);
+// }
