f3dfd12463ec3afa9a93a921e47b3dfeaa461d87
unknown <baudin-thomas@sfr.fr>
2026-02-19 10:56:17 +0100

    idem

--- tests/text/test_textcomp.cpp
+++ tests/text/test_textcomp.cpp
@@ -19,8 +19,47 @@
 
 #include "text/TextComp.hpp"
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 TEST_CASE("Textcomp", "[textcomp]")
 {
-    
+    const auto text1 = segment01::TextComp();
+    auto text2 = text1;
+    text2 = text1;
+    text2.setCharacterSize(segment01::InfoText::DefaultSize);
+    text2.setCharacterSize(segment01::InfoText::DefaultSize - 1);
+    text2.setLetterSpacing(false, 0);
+    text2.setLetterSpacing(false, 0);
+    text2.setLineSpacing(0);
+    text2.setLineSpacing(0);
+    text2.setStyle(segment01::InfoText::Style::BOLD);
+    text2.setStyle(segment01::InfoText::Style::BOLD);
+    text2.setFillColor(sf::Color(0, 0, 0, segment01::Constant::MaxColor));
+    text2.setFillColor(sf::Color(0, 0, 0, segment01::Constant::MaxColor));
+    text2.setOutlineColor(sf::Color(0, 0, 0, 0));
+    text2.setOutlineColor(sf::Color(0, 0, 0, 0));
+    text2.setOutlineThickness(1);
+    text2.setOutlineThickness(1);
+    text2.setWidthWrap(1);
+    text2.setWidthWrap(1);
+    REQUIRE(text2.getChainText().empty());
+    REQUIRE(text2.getFont() == nullptr);
+    REQUIRE(text2.getCharacterSize() == segment01::InfoText::DefaultSize - 1);
+    REQUIRE(!std::get<0>(text2.getLetterSpacing()));
+    REQUIRE_THAT(
+        std::get<1>(text2.getLetterSpacing()),
+        Catch::Matchers::WithinRel(0, segment01::Constant::m_epsilon_f));
+    REQUIRE_THAT(
+        text2.getLineSpacing(),
+        Catch::Matchers::WithinRel(0, segment01::Constant::m_epsilon_f));
+    REQUIRE(text2.getStyle() == segment01::InfoText::Style::BOLD);
+    REQUIRE(text2.getFillColor() ==
+            sf::Color(0, 0, 0, segment01::Constant::MaxColor));
+    REQUIRE(text2.getOutlineColor() == sf::Color(0, 0, 0, 0));
+    REQUIRE_THAT(
+        text2.getOutlineThickness(),
+        Catch::Matchers::WithinRel(1, segment01::Constant::m_epsilon_f));
+    REQUIRE_THAT(
+        text2.getWidthWrap(),
+        Catch::Matchers::WithinRel(1, segment01::Constant::m_epsilon_f));
 }
